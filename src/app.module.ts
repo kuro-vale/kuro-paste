687544c396a8d0c6f4245dea4b61b07a2ee6e93f
kuro-vale <jsalcedo218@gmail.com>
2022-11-11 19:05:26 -0500

    Simplify app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -12,6 +11,5 @@ import { MongooseModule } from '@nestjs/mongoose';
     MongooseModule.forRoot(process.env.MONGODB_URL),
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
